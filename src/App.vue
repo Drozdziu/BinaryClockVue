b3fba1a909f6e3d8a1aee111cc3d4192e5397896
Drozdziu <kubawanat625@gmail.com>
2025-09-09 22:37:30 +0200

    SetupProject
    Added components that represents binary clock

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import binaryNumber from './components/binaryNumber.vue'
+</script>
+
+<template>
+  <div class="container">
+    <binary-number msg="2"/>
+    <binary-number msg="4"/>
+    <binary-number msg="3"/>
+    <binary-number msg="4"/>
+    <binary-number msg="3"/>
+    <binary-number msg="4"/>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 10%;
+  display: flex ;
+  justify-content: center; /* ustawia na środku w poziomie */
+}
+</style>
---
+++ src/components/binaryNumber.vue
@@ -0,0 +1,34 @@
+<script>
+    export default {
+    name: 'binaryNumber ',
+    props: {
+        msg: String
+    },
+    data() { // zmienne
+        const time = new Date()
+        return {
+            hours: time.getHours(),
+            minutes: time.getMinutes(),
+            seconds: time.getSeconds(),
+        }
+    },
+    methods: { }// metody
+}
+</script>
+<template>
+<div id="container"> 
+    <div class="number"></div>
+    <div class="number"></div>
+    <div class="number" v-if="msg === '3' || msg === '4'"></div>
+    <div class="number"v-if="msg === '4'"></div> 
+</div>
+</template>
+
+<style scoped>
+    .number{
+        background-color: black;
+        height: 100px;
+        width: 100px;
+        margin: 10px;
+    }
+</style>
