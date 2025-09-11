db9663fa101d580dc5bba641c4c5d3811440685d
Drozdziu <kubawanat625@gmail.com>
2025-09-11 15:16:20 +0200

    Provisory lightup
    Added logic to lightup the first of divs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 import binaryNumber from './components/binaryNumber.vue'
+// const time = new Date()
+let hours = 8//time.getHours()
+let minutes = 12//time.getMinutes()
+let seconds = 24//time.getSeconds()
 </script>
 
 <template>
   <div class="container">
-    <binary-number msg="2"/>
-    <binary-number msg="4"/>
-    <binary-number msg="3"/>
-    <binary-number msg="4"/>
-    <binary-number msg="3"/>
-    <binary-number msg="4"/>
+    <binary-number :msg="2" :time="hours"/>
+    <binary-number :msg="4" :time="hours"/>
+    <binary-number :msg="3" :time="minutes"/>
+    <binary-number :msg="4" :time="minutes"/>
+    <binary-number :msg="3" :time="seconds"/>
+    <binary-number :msg="4" :time="seconds"/>
   </div>
 </template>
 
--- src/components/binaryNumber.vue
+++ src/components/binaryNumber.vue
@@ -1,26 +1,48 @@
 <script>
     export default {
-    name: 'binaryNumber ',
+    name: 'binary-number',
     props: {
-        msg: String
+        msg: Number,
+        time: Number,
+        //testNumber: 0  tak nie robimy
+        testNumber: {
+            type: Number,
+            default: 0
+        }  
     },
-    data() { // zmienne
-        const time = new Date()
-        return {
-            hours: time.getHours(),
-            minutes: time.getMinutes(),
-            seconds: time.getSeconds(),
+    data(){
+        return{
+            testTime: this.testNumber,
         }
     },
-    methods: { }// metody
+    methods: {
+        test(){
+            console.log(this.testTime)
+            this.testTime+=1
+            this.lightup()
+        },
+        lightup(){
+            const boxes = this.$el.getElementsByClassName('number') // this.$el - tylko w tym komponencie
+            console.log(boxes)
+            if(this.testTime % 2 == 1 && boxes[0]) boxes[0].style.backgroundColor = "white"
+            else boxes[0].style.backgroundColor = "black"
+            // if(this.testTime >= 8 && boxes[3]) boxes[3].style.backgroundColor = "white"
+        }
+     },
+    mounted() {
+        this.test()
+        this.lightup()
+    }
 }
 </script>
 <template>
 <div id="container"> 
+    <button @click="test">test</button>
     <div class="number"></div>
     <div class="number"></div>
-    <div class="number" v-if="msg === '3' || msg === '4'"></div>
-    <div class="number"v-if="msg === '4'"></div> 
+    <div class="number" v-if="msg >= 3"></div>
+    <div class="number"v-if="msg >= 4"></div>
+    
 </div>
 </template>
 
@@ -30,5 +52,7 @@
         height: 100px;
         width: 100px;
         margin: 10px;
+        transition: 200ms;
+        border-radius: 10px;
     }
 </style>
