bd6449927d042f22ca812ac6c7cd390e0a38b9cf
Drozdziu <kubawanat625@gmail.com>
2025-09-12 16:04:36 +0200

    Reactive clock
    Added reactive variables and now binary clock follows the actual time

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <script setup>
 import binaryNumber from './components/binaryNumber.vue'
-// const time = new Date()
-let hours = 8//time.getHours()
-let minutes = 12//time.getMinutes()
-let seconds = 24//time.getSeconds()
+import { computed, ref } from 'vue'
+
+const hours = ref(0)
+const minutes = ref(0)
+const seconds = ref(0)
+let hour1 = computed(()=>Math.floor(hours.value/10) ) , hour2 = computed(()=>hours.value%10)
+let minutes1 =  computed(()=>Math.floor(minutes.value/10)), minutes2 = computed(()=> minutes.value%10)
+let seconds1 = computed(()=>Math.floor(seconds.value/10)), seconds2 = computed(()=>Math.floor(seconds.value%10))
+  
+function updateTime() {
+  const now = new Date()
+  hours.value = now.getHours()
+  minutes.value = now.getMinutes()
+  seconds.value = now.getSeconds()
+}
+
+setInterval(updateTime, 1000)
+updateTime()
 </script>
 
 <template>
   <div class="container">
-    <binary-number :msg="2" :time="hours" :limit="2"/>
-    <binary-number :msg="4" :time="hours" :limit="9"/>
-    <binary-number :msg="3" :time="minutes" :limit="5"/>
-    <binary-number :msg="4" :time="minutes" :limit="9"/>
-    <binary-number :msg="3" :time="seconds" :limit="5"/>
-    <binary-number :msg="4" :time="seconds" :limit="9"/>
+    <binary-number :msg="2" :time="hour1" :limit="2"/>
+    <binary-number :msg="4" :time="hour2" :limit="9"/>
+    <binary-number :msg="3" :time="minutes1" :limit="5"/>
+    <binary-number :msg="4" :time="minutes2" :limit="9"/>
+    <binary-number :msg="3" :time="seconds1" :limit="5"/>
+    <binary-number :msg="4" :time="seconds2" :limit="9"/>
   </div>
 </template>
 
--- src/components/binaryNumber.vue
+++ src/components/binaryNumber.vue
@@ -18,54 +18,61 @@
     },
     methods: {
         test(){
-            console.log(this.testTime)
-            this.testTime+=1
-            if(this.testTime > this.limit) this.testTime = 0
+            console.log(this.time)
             this.lightup()
         },
         lightup(){
             const boxes = this.$el.getElementsByClassName('number') // this.$el - tylko w tym komponencie
             console.log(boxes)
-            if(this.testTime % 2 == 1 && this.testTime != 0) boxes[0].style.backgroundColor = "white"
+            if(this.time % 2 == 1 && this.time != 0) boxes[0].style.backgroundColor = "white"
             else boxes[0].style.backgroundColor = "black"
 
-            if(this.testTime % 4 == 2 || this.testTime % 4 == 3) boxes[1].style.backgroundColor = "white"
+            if(this.time % 4 == 2 || this.time % 4 == 3) boxes[1].style.backgroundColor = "white"
             else boxes[1].style.backgroundColor = "black"
-            // if(msg == 3){
-            if(this.testTime >= 4 && this.testTime <= 7) boxes[2].style.backgroundColor = "white"
-            else boxes[2].style.backgroundColor = "black"
-            // }
-            // if(msg == 4){
-            if(this.testTime >= 8)boxes[3].style.backgroundColor = "white"
-            else boxes[3].style.backgroundColor = "black"
-            // } 
+            if(this.msg == 3){
+                if(this.time >= 4 && this.time <= 7) boxes[2].style.backgroundColor = "white"
+                else boxes[2].style.backgroundColor = "black"
+            }
+            if(this.msg == 4){
+                if(this.time >= 8)boxes[3].style.backgroundColor = "white"
+                else boxes[3].style.backgroundColor = "black"
+            } 
         }
      },
     mounted() {
         this.test()
+        this.lightup()
+    },
+    updated() {
         this.lightup()
     }
 }
 </script>
 <template>
 <div id="container"> 
-    <button @click="test">test</button>
-    <div class="number"></div>
-    <div class="number"></div>
     <div class="number" v-if="msg >= 3"></div>
+    <div class="empty" v-else></div>
     <div class="number"v-if="msg >= 4"></div>
-    <p> {{ this.testTime }}</p>
+    <div class="empty" v-else></div>
+    <div class="number"></div>
+    <div class="number"></div>
+
+    <p> {{ this.time }}</p>
 </div>
 </template>
 
 <style scoped>
-    .number{
+    .number, .empty{
         background-color: black;
         height: 100px;
         width: 100px;
         margin: 10px;
         transition: 200ms;
         border-radius: 10px;
+    }
+    .number{
+        background-color: black;
+        transition: 200ms;
     }
     p{
         color: #fff;
