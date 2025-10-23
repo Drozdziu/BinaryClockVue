11f77b10b1c60de5ee5ba80bd9428d2f3472eea8
Drozdziu <kubawanat625@gmail.com>
2025-10-23 14:55:35 +0200

    Demo fixes
    Fixed background color in demo

--- src/App.vue
+++ src/App.vue
@@ -29,21 +29,6 @@ export default {
     setInterval(this.updateTime, 1000);
   }
 } 
-// import { computed, ref } from 'vue'
-// const hours = ref(0)
-// const minutes = ref(0)
-// const seconds = ref(0)
-// let hour1 = computed(() => Math.floor(hours.value / 10)), hour2 = computed(() => hours.value % 10)
-// let minutes1 = computed(() => Math.floor(minutes.value / 10)), minutes2 = computed(() => minutes.value % 10)
-// let seconds1 = computed(() => Math.floor(seconds.value / 10)), seconds2 = computed(() => seconds.value % 10)
-// function updateTime() {
-//   const now = new Date()
-//   hours.value = now.getHours()
-//   minutes.value = now.getMinutes()
-//   seconds.value = now.getSeconds()
-// }
-// setInterval(updateTime, 1000)
-// updateTime()
 </script>
 
 <template>
